99376e2c94180c2076eb90bf2643007d1778fa1d
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-25 12:50:56 -0400

    Added win combination and initialize board

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 require 'colorize'
 
+WIN_COMBINATIONS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+  [0, 4, 8], [2, 4, 6]             # Diagonals
+]
+
 # * colorizing methods
 
 def blue_color(string)
@@ -20,4 +26,10 @@ end
 
 def gray_color(string)
   string.colorize(:gray)
+end
+
+
+# we need to wrap in method cuz this array with refresh every time by chosen yes
+def initialize_board
+  Array.new(9, ' ')
 end
