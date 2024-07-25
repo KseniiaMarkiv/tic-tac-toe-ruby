92798157ca87082918e1bdf2588922fd418bb364
KseniiaMarkiv <ksenya.markiv@gmail.com>
2024-07-25 12:50:05 -0400

    Added main file for tic-tac-toe

---
+++ main.rb
@@ -0,0 +1,23 @@
+require 'colorize'
+
+# * colorizing methods
+
+def blue_color(string)
+  string.colorize(:blue)
+end
+
+def yellow_color(string)
+  string.colorize(:yellow)
+end
+
+def green_color(string)
+  string.colorize(:green)
+end
+
+def red_color(string)
+  string.colorize(:red)
+end
+
+def gray_color(string)
+  string.colorize(:gray)
+end
